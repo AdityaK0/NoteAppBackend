57efda54e6a86888903349c2bf79d2bd12adb399
AdityaK0 <aditya123905@gmail.com>
2025-03-29 00:02:39 +0530

    config

--- backend/settings.py
+++ backend/settings.py
@@ -126,8 +126,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 DATABASES = {
-'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        engine='django.db.backends.postgresql'
+    )
 }
 
 
