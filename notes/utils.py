394372c8562a37d43aa81a8d48e44be5008a5777
AdityaK0 <aditya123905@gmail.com>
2025-08-29 12:45:47 +0530

    handling  data inconsistency when redis server is off

--- notes/utils.py
+++ notes/utils.py
@@ -1,6 +1,10 @@
 from django.core.cache import cache
 
-def clear_user_notes_cache(user_id):
-    key = f"notes-{user_id}"
-    cache.delete(key)
-    print(f"[CACHE] Cleared notes cache for user {key}")
+def clear_user_notes_cache(user_id: int):
+    cache_key = f"notes-{user_id}"
+    try:
+        cache.delete(cache_key)
+        print(f"Cache cleared for {cache_key}")
+    except Exception as e:
+        #  just skip, don’t crash signals
+        print(f"Cache clear failed (Redis down?): {e}")
