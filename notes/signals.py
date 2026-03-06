1eae59f6f5e4cdaebce0312cf7d611c341edff1b
AdityaK0 <aditya123905@gmail.com>
2026-03-06 20:11:51 +0530

    remove signals

--- notes/signals.py
+++ notes/signals.py
@@ -1,18 +1,18 @@
-from django.db.models.signals import post_save,post_delete
-from django.contrib.auth.models import User
-from django.dispatch import receiver
-from .models import Notes
-from django.core.cache import cache
-from .utils import clear_user_notes_cache
+# from django.db.models.signals import post_save,post_delete
+# from django.contrib.auth.models import User
+# from django.dispatch import receiver
+# from .models import Notes
+# from django.core.cache import cache
+# from .utils import clear_user_notes_cache
 
 
-@receiver(post_save, sender=Notes)
-def create_user_profile(sender, instance, created, **kwargs):
-        clear_user_notes_cache(instance.user.id)
-        print("RESETING CACHE VALUES ::::: ")
+# @receiver(post_save, sender=Notes)
+# def create_user_profile(sender, instance, created, **kwargs):
+#         clear_user_notes_cache(instance.user.id)
+#         print("RESETING CACHE VALUES ::::: ")
         
-@receiver(post_delete, sender=Notes)
-def clear_notes_cache_on_delete(sender, instance, **kwargs):
-    # Called when a note is deleted
-    clear_user_notes_cache(instance.user.id)
-    print("CACHE CLEARED on DELETE")
+# @receiver(post_delete, sender=Notes)
+# def clear_notes_cache_on_delete(sender, instance, **kwargs):
+#     # Called when a note is deleted
+#     clear_user_notes_cache(instance.user.id)
+#     print("CACHE CLEARED on DELETE")
